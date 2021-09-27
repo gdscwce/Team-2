e4213e0682594c96d17eb47c28cf5a5f93bdb862
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-09-27 16:56:06 +0530

    Firebase Auth Login Page Demo

---
+++ Firebase Auth/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBczU5fIIPY3lPApG7TopgdK9ymCWhYh00",
+    authDomain: "login-demo-app-362c1.firebaseapp.com",
+    projectId: "login-demo-app-362c1",
+    storageBucket: "login-demo-app-362c1.appspot.com",
+    messagingSenderId: "811582463381",
+    appId: "1:811582463381:web:64f6efba735e120d432a9c",
+    measurementId: "G-JSJ3YD2GQ8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
